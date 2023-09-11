fe011987e02622aa41608ca75ac2f30b1543d720
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2023-09-11 13:38:45 +0000

    updated error page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Cart from './CartPage'
 import Error from './ErrorPage'
 import Checkout from './CheckoutPage'
 import PrivateRoute from './PrivateRoute'
+import AuthWrapper from './AuthWrapper'
 export {
   Home,
   SingleProduct,
