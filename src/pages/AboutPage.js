77d7895ad19e68e4c665dec63622667ff25b6c6e
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2023-09-13 13:56:06 +0000

    Update aboutpage and homepage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -13,7 +13,7 @@ const AboutPage = () => {
     <h2>our story</h2>
     <div className="underline"></div>
   </div>
-  <p>Lorem ipsum</p>
+  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
 </article>
     </Wrapper>
   </main>
