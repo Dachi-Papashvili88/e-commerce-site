fa91ece3ec82220e2d7b409bbf82242615606cf8
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2023-09-13 17:50:41 +0000

    Update services and contact components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return <Wrapper className="section-center">
+    <h3>Join out newsletter and get 20% off</h3>
+    <div className='content'>
+      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <form className='contact-form'>
+       <input type="email" className='form-input' placeholder='enter email'/>
+       <button type="submit" className='submit-btn'>subscribe</button>
+    </form>
+    </div>
+  </Wrapper>
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
