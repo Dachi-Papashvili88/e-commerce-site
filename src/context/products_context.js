2b873fe04f960741abbc4cd52942738807c48797
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2023-09-20 13:07:13 +0000

    created featured products content

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -7,10 +7,7 @@ import {
   SIDEBAR_CLOSE,
   GET_PRODUCTS_BEGIN,
   GET_PRODUCTS_SUCCESS,
-  GET_PRODUCTS_ERROR,
-  GET_SINGLE_PRODUCT_BEGIN,
-  GET_SINGLE_PRODUCT_SUCCESS,
-  GET_SINGLE_PRODUCT_ERROR,
+  GET_PRODUCTS_ERROR
 } from '../actions'
 
 const initialState = {
@@ -18,7 +15,7 @@ const initialState = {
   products_loading: false, 
   products_error: false, 
   products: [], 
-  featured_products:[]
+  featured_products:[],
 }
 
 const ProductsContext = React.createContext()
@@ -35,14 +32,14 @@ export const ProductsProvider = ({ children }) => {
   }
 
   const fetchProducts = async(url) =>{
-    dispatch({type: GET_PRODUCTS_BEGIN})
+    dispatch({type:GET_PRODUCTS_BEGIN})
     try {
       const response = await axios.get(url)
       const products = response.data
       dispatch({type: GET_PRODUCTS_SUCCESS, payload: products})
 
     } catch(error) {
-      dispatch({type:GET_SINGLE_PRODUCT_ERROR})
+      dispatch({type:GET_PRODUCTS_ERROR})
   }
 }
 
